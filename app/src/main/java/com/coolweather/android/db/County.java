b2b67c5412f46caf1e99dbf931060fcf7463884c
Tony <yuloi168.gmail.com>
2020-05-16 22:20:46 +0800

    add to database and table config.

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,47 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 16/5/2020.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getCountryName(){
+        return countyName;
+    }
+
+    public void setCountryName(String countryName){
+        this.countyName = countryName;
+    }
+
+    public String getWeatherId(){
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId){
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId(){
+        return cityId;
+    }
+
+    public void setCityId(int cityId){
+        this.cityId = cityId;
+    }
+
+} // end of class County
---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,38 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 16/5/2020.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+} // end of class Province
